b69a1e38018a7c59c24f7fa564084d3c6531b28f
TBFighter Tofu <leonmuss4@gmail.com>
2026-01-17 20:44:09 -0800

    search for more 4-year awards

--- run.py
+++ run.py
@@ -10,8 +10,10 @@ from src.awards import AwardSearchDownload
 tas_codes = []
 for year in [2024, 2023, 2022, 2021, 2020, 2019]:
     tas_codes.append(f"072-019-{year}/{year+1}-1031-000")
-for year in [2024]:
+for year in [2024, 2023, 2022, 2021]:
     tas_codes.append(f"072-019-{year}/{year+4}-1031-000")
+for year in [2024, 2023, 2022, 2021, 2020]:
+    tas_codes.append(f"072-019-{year}/{year+5}-1031-000")
 for year in [2022, 2021, 2020, 2019, 2018]:
     tas_codes.append(f"072-{year}/{year+1}-1021-000")
     tas_codes.append(f"072-{year}/{year+5}-1021-000")
@@ -25,4 +27,3 @@ for tas_code in tas_codes:
     )  # an object that can download the data you want from usaspending
 
     downloader.run_all()
-
